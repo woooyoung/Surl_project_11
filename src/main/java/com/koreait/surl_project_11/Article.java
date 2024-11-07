f9d0a558d7fa8f2dafcbad3e2d797c9fb26c758d
woooyoung <axdsw121@gmail.com>
2024-11-07 15:46:37 +0900

    JPA가 실행하는 SQL 로깅

--- src/main/java/com/koreait/surl_project_11/Article.java
+++ src/main/java/com/koreait/surl_project_11/Article.java
@@ -1,5 +1,6 @@
 package com.koreait.surl_project_11;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,5 +13,9 @@ public class Article {
     @GeneratedValue(strategy = IDENTITY)
     private long id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String body;
+    private String body2;
+    private String body3;
+    private String body4;
 }
