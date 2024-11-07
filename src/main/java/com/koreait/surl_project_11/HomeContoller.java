71cb3363c6bcf1ac847400e1989039161a1df568
woooyoung <axdsw121@gmail.com>
2024-11-07 11:13:58 +0900

    Github Action Test

--- src/main/java/com/koreait/surl_project_11/HomeContoller.java
+++ src/main/java/com/koreait/surl_project_11/HomeContoller.java
@@ -9,10 +9,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeContoller {
     @Value("${custom.site.name}")
     private String customSiteName;
+    @Value("${custom.secret.key}")
+    private String secretKey;
 
     @GetMapping("/")
     @ResponseBody
     public String ShowMain() {
-        return "Main on " + customSiteName;
+        return "123Main on " + customSiteName;
+    }
+
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String ShowSecretKey() {
+        return "secretKey : " + secretKey;
     }
 }
