64f5d1cf96d018095b8bec0f3043104b23268f63
woooyoung <axdsw121@gmail.com>
2024-11-08 10:34:48 +0900

    게시글 COUNT, DELETE, TRUNCATE

--- src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
@@ -4,14 +4,18 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
     @Id
     @GeneratedValue(strategy = IDENTITY)
--- src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
@@ -18,26 +18,24 @@ public class NotProd {
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
     public ApplicationRunner initNotProd() {
         return args -> {
-            System.out.println("Not Prod.initNotProd1");
-            System.out.println("Not Prod.initNotProd2");
-            System.out.println("Not Prod.initNotProd3");
 
-            Article articleFirst = Article.builder().
+            if (articleRepository.count() > 0) return;
+
+            //articleRepository.deleteAll();
+
+            Article article1 = Article.builder().
                     title("제목1")
                     .body("내용1").build();
 
-            Article articleSecond = Article.builder().
+            Article article2 = Article.builder().
                     title("제목2")
                     .body("내용2").build();
 
-            System.out.println(articleFirst.getId());
-            System.out.println(articleSecond.getId());
 
-            articleRepository.save(articleFirst);
-            articleRepository.save(articleSecond);
+            articleRepository.save(article1);
+            articleRepository.save(article2);
 
-            System.out.println(articleFirst.getId());
-            System.out.println(articleSecond.getId());
+
         };
     }
 }
