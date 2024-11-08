98bd4f5346894acb338bcb2f9fb5544427065255
woooyoung <axdsw121@gmail.com>
2024-11-08 10:56:12 +0900

    게시글 UPDATE

--- src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
@@ -4,16 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Article {
--- src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
+++ src/main/java/com/koreait/surl_project_11/global/initData/NotProd.java
@@ -2,7 +2,9 @@ package com.koreait.surl_project_11.global.initData;
 
 import com.koreait.surl_project_11.domain.article.article.entity.Article;
 import com.koreait.surl_project_11.domain.article.article.repository.ArticleRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +19,30 @@ public class NotProd {
 
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
     public ApplicationRunner initNotProd() {
-        return args -> {
+        return new ApplicationRunner(){
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+                if (articleRepository.count() > 0) return;
 
-            if (articleRepository.count() > 0) return;
+                //articleRepository.deleteAll();
 
-            //articleRepository.deleteAll();
+                Article article1 = Article.builder().
+                        title("제목1")
+                        .body("내용1").build();
 
-            Article article1 = Article.builder().
-                    title("제목1")
-                    .body("내용1").build();
-
-            Article article2 = Article.builder().
-                    title("제목2")
-                    .body("내용2").build();
-
-
-            articleRepository.save(article1);
-            articleRepository.save(article2);
+                Article article2 = Article.builder().
+                        title("제목2")
+                        .body("내용2").build();
 
 
+                articleRepository.save(article1);
+                articleRepository.save(article2);
+
+                article2.setTitle("제목 2-2");
+
+                articleRepository.delete(article1);
+            }
         };
     }
 }
