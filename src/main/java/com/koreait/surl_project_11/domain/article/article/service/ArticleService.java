d5b0a52454a1542a55f8cb2aee9f837fea2f855f
woooyoung <axdsw121@gmail.com>
2024-11-08 15:26:09 +0900

    서비스에 @Transactional 적용

--- src/main/java/com/koreait/surl_project_11/domain/article/article/service/ArticleService.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/service/ArticleService.java
@@ -5,12 +5,14 @@ import com.koreait.surl_project_11.domain.article.article.repository.ArticleRepo
 import com.koreait.surl_project_11.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ArticleService {
     private final ArticleRepository articleRepository;
 
@@ -22,6 +24,7 @@ public class ArticleService {
     // - 이번에 생성된 게시글의 번호
     // - 게시글 생성에 대한 결과 메세지
     // - 결과 코드
+    @Transactional
     public RsData<Article> write(String title, String body) {
         Article article = Article
                 .builder()
@@ -34,6 +37,7 @@ public class ArticleService {
         return RsData.of("%d번 게시글이 생성됨".formatted(article.getId()), article);
     }
 
+    @Transactional
     public void delete(Article article) {
         articleRepository.delete(article);
     }
--- src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/service/MemberService.java
@@ -6,21 +6,23 @@ import com.koreait.surl_project_11.global.eceptions.GlobalException;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    private Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional
+    // @Transactional(noRollbackFor = GlobalException.class)
     public RsData<Member> join(String username, String password, String nickname) {
-        //boolean present = findByUsername(username).isPresent();
-
-        //if (present) {
-        //    throw new GlobalException("400-1", "이미 존재하는 아이디야");
-        //}
-
         findByUsername(username).ifPresent(ignored -> {
             throw new GlobalException("400-1", "이미 존재하는 아이디야");
         });
@@ -34,7 +36,5 @@ public class MemberService {
         return RsData.of("회원가입이 완료되었습니다.", member);
     }
 
-    private Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
-    }
+
 }
