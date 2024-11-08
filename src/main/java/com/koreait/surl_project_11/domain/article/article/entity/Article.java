464ad49b5c98b17ba9a551c0d695919697776597
woooyoung <axdsw121@gmail.com>
2024-11-08 15:39:21 +0900

    MappedSuperClass

--- src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/surl_project_11/domain/article/article/entity/Article.java
@@ -1,30 +1,22 @@
 package com.koreait.surl_project_11.domain.article.article.entity;
 
-import jakarta.persistence.*;
+import com.koreait.surl_project_11.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Builder
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class Article {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Article extends BaseTime {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
--- src/main/java/com/koreait/surl_project_11/domain/member/member/entity/Member.java
+++ src/main/java/com/koreait/surl_project_11/domain/member/member/entity/Member.java
@@ -1,31 +1,18 @@
 package com.koreait.surl_project_11.domain.member.member.entity;
 
-import jakarta.persistence.*;
+import com.koreait.surl_project_11.global.jpa.entity.BaseTime;
+import jakarta.persistence.Entity;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Member {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
-    @Column(unique = true)
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+public class Member extends BaseTime {
     private String username;
     private String password;
     private String nickname;
