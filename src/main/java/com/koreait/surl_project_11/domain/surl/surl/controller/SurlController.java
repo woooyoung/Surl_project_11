03fc0943e9b5834da7cf9ca386c064d029da5d6b
woooyoung <axdsw121@gmail.com>
2024-11-11 15:41:10 +0900

    DB test

--- src/main/java/com/koreait/surl_project_11/domain/surl/surl/controller/SurlController.java
+++ src/main/java/com/koreait/surl_project_11/domain/surl/surl/controller/SurlController.java
@@ -8,6 +8,7 @@ import com.koreait.surl_project_11.global.rq.Rq;
 import com.koreait.surl_project_11.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class SurlController {
 
     private final Rq rq;
@@ -28,13 +30,6 @@ public class SurlController {
 
         Member member = rq.getMember(); // 현재 브라우저로 로그인 한 회원 정보
 
-        System.out.println("before get id");
-        member.getId();
-        System.out.println("after get id");
-
-        System.out.println("before get username");
-        member.getUsername();
-        System.out.println("after get username");
 
         return surlService.add(member, body, url);
     }
