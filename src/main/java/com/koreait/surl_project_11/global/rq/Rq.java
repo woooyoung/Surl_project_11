96cc4bd1da1732813c4e137b8574b66cef278de9
woooyoung <axdsw121@gmail.com>
2024-11-26 13:44:26 +0900

    쿠키

--- src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
+++ src/main/java/com/koreait/surl_project_11/global/rq/Rq.java
@@ -4,11 +4,14 @@ import com.koreait.surl_project_11.domain.member.member.entity.Member;
 import com.koreait.surl_project_11.domain.member.member.service.MemberService;
 import com.koreait.surl_project_11.global.exceptions.GlobalException;
 import com.koreait.surl_project_11.standard.util.Ut;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.Arrays;
 
 @Component
 @RequestScope
@@ -27,8 +30,11 @@ public class Rq {
     public Member getMember() {
         if (member != null) return member; // 메모리 캐싱
 
-        String actorUsername = req.getParameter("actorUsername");
-        String actorPassword = req.getParameter("actorPassword");
+        String actorUsername = getCookieValue("actorUsername", null);
+        String actorPassword = getCookieValue("actorPassword", null);
+
+//        String actorUsername = req.getParameter("actorUsername");
+//        String actorPassword = req.getParameter("actorPassword");
 
         if (actorUsername == null || actorPassword == null) {
             String authorization = req.getHeader("Authorization");
@@ -52,6 +58,14 @@ public class Rq {
         member = loginedMember;
 
         return loginedMember;
+    }
+
+    private String getCookieValue(String cookieName, String defaultValue) {
+        return Arrays.stream(req.getCookies()) // 쿠키 배열을 스트림으로 변환
+                .filter(cookie -> cookie.getName().equals(cookieName))// 쿠키의 이름이 매개변수로 쓰이게
+                .findFirst() // 첫 번째 요소
+                .map(Cookie::getValue) // 존재하면 쿠키 값으로 매핑
+                .orElse(defaultValue); // 없으면 기본 값
     }
 
     public String getCurrentUrlPath() {
