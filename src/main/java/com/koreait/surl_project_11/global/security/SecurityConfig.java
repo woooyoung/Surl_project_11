f83b039f19672834e006bc0b8e66ec42883a369a
woooyoung <axdsw121@gmail.com>
2024-11-26 15:53:10 +0900

    CustomAuthenticationFilter 추가

--- src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
+++ src/main/java/com/koreait/surl_project_11/global/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.koreait.surl_project_11.global.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,7 +37,7 @@ public class SecurityConfig {
                 ) // 타임리프, MPA에서는 csrf를 사용함 // REST API 방식에서는 끈다
                 .formLogin(formLogin ->
                         formLogin.permitAll()
-                );
+                ).addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
